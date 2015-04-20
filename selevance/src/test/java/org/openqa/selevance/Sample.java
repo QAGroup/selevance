603b307179fe4362db9da74aa06a18456fbec31e
sarkartanmay <tanmayit08@gmail.com>
2015-04-20 17:52:35 +0530

    Updated with "Sample" example

--- selevance/src/test/java/org/openqa/selevance/Sample.java
+++ selevance/src/test/java/org/openqa/selevance/Sample.java
@@ -2,8 +2,13 @@ package org.openqa.selevance;
 
 import java.util.HashMap;
 
+import org.openqa.selenium.By;
 import org.openqa.selevance.data.DataReader;
 import org.openqa.selevance.data.DataReader.SELEVANCEDATA;
+import org.openqa.selevance.util.Util;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 /**
@@ -12,6 +17,15 @@ import org.testng.annotations.Test;
  */
 public class Sample {
 	
+	protected String globalConfig;
+	protected String baseUrl;
+	
+	@BeforeMethod
+	@Parameters({ "propconfig" })
+	void setBrowser(@Optional String config){
+		globalConfig = config;		
+	}
+	
 	@Test(description = "Sample Description", 
 			dataProviderClass= DataReader.class,
 			dataProvider = "EXCEL" )
@@ -19,6 +33,14 @@ public class Sample {
 			sheet ="Sheet2" , format ="FIRSTYES")
 	public void doTest(HashMap<String, String> testdata){
 		System.out.println(testdata);
+		String name = testdata.get("Fname") + " " +testdata.get("Lname") ;
+		baseUrl = "http://google.co.in";		
+		Plus plus = new Plus(globalConfig);
+		plus.driver().get(baseUrl);
+		plus.driver().findElement(By.xpath(".//*[@id='lst-ib']")).clear();
+		plus.driver().findElement(By.xpath(".//*[@id='lst-ib']")).sendKeys(name);
+		Util.sleep(5000);
+		plus.driver().quit();
 	}
 
 }
