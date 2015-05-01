7046dde365f53f057649a464d483410482eb4c0b
sarkartanmay <tanmayit08@gmail.com>
2015-05-01 19:01:29 +0530

    Updated with URL Validation. close #8

--- selevance/src/main/java/org/openqa/selevance/PlusElement.java
+++ selevance/src/main/java/org/openqa/selevance/PlusElement.java
@@ -1,5 +1,9 @@
 package org.openqa.selevance;
 
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Request;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -32,5 +36,21 @@ public class PlusElement {
 		WebDriverWait wait = new WebDriverWait(driver, 10);
 	    wait.until(ExpectedConditions.visibilityOf(element));
 	    return element;
+	}
+	public int httpStatus(String url){
+		int status = 0;
+		try {
+			status = Request.Get(url).execute().returnResponse().getStatusLine().getStatusCode();
+		}catch (ClientProtocolException e) {			
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}return status;
+	}
+	public boolean urlPresent(String url){
+		if(httpStatus(url)==404){
+			return true;
+		}else
+			return false;
 	}
 }
